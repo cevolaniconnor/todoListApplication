5e8e8fce195341a66b8bae1f3a4306555299504a
cevolaniconnor <70539453+cevolaniconnor@users.noreply.github.com>
2024-12-05 14:29:20 -0800

    Added basic menu system with user input

--- main.cpp
+++ main.cpp
@@ -2,6 +2,16 @@
 using namespace std;
 
 int main(){
+
+	int input;
+
+	cout << "Todo List" << endl;
+	cout << "1) Add a task" << endl;
+	cout << "2) View all tasks" << endl;
+	cout << "3) Delete task (By index)" << endl;
+	cout << "4) Mark task completed" << endl;
+
+	cin >> input;
 	
 	return 0;
 }
